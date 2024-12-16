b9c8119ee58e44a25314385b5317bd6df06ed45d
Nandan Kumar <maachandi2@gmail.com>
2024-12-16 13:59:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Web Developer", "Fullstack Developer"],
+    strings: ["Front-End Developer", "Web Developer", "Full Stack Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
